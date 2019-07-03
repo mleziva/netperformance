97c6ee2179478c6061fb6365343d95c77480c7f7
Austin Mleziva <ausmle@microsoft.com>
2019-07-03 11:37:09 -0500

    Adding powershell scripts for creating a cosmos db account and initial commit of settings class

---
+++ angularsample/PowerShell/Azure-CosmosDb-Create.ps1
@@ -0,0 +1,60 @@
+﻿<# Create an Azure Cosmos Account for Core (SQL) API
+
+requirements:
+Azure powershll
+https://docs.microsoft.com/en-us/powershell/azure/install-az-ps?view=azps-2.4.0#requirements
+
+#ensure we're logged in
+try {
+    $ctx=Get-AzureRmContext -ErrorAction Stop
+}
+catch {
+    Login-AzureRmAccount -SubscriptionId $subscriptionId -TenantId $tenantId -ErrorAction Stop
+}
+if($ctx.Subscription.TenantId -ne $tenantId){
+    Login-AzureRmAccount -SubscriptionName $subscriptionId -TenantId $tenantId -ErrorAction Stop
+}
+
+#>
+Param(
+    $Tenant,
+    $SubscriptionId,
+    $ResourceGroupName= "PerfTestResourceGroup",
+    $Location= "West US",
+    $AccountName = "austintestaccount"
+)
+
+Connect-AzAccount -Tenant $Tenant -SubscriptionId $SubscriptionId
+
+try{
+	Get-AzResourceGroup -Name $ResourceGroupName -ErrorAction Stop
+	Write-Host "Resource group $ResourceGroupName exists"
+}
+catch{
+	Write-Host "Started Creating resource group $ResourceGroupName"
+	New-AzResourceGroup -Name $ResourceGroupName -Location $Location
+	Write-Host "Finished Creating resource group $ResourceGroupName"
+}
+$locations = @(
+    @{ "locationName"="West US"; "failoverPriority"=0 },
+    @{ "locationName"="East US"; "failoverPriority"=1 }
+)
+
+$consistencyPolicy = @{
+    "defaultConsistencyLevel"="BoundedStaleness";
+    "maxIntervalInSeconds"=300;
+    "maxStalenessPrefix"=100000
+}
+
+$CosmosDBProperties = @{
+    "databaseAccountOfferType"="Standard";
+    "locations"=$locations;
+    "consistencyPolicy"=$consistencyPolicy;
+    "enableMultipleWriteLocations"="true"
+}
+
+Write-Host "Started Creating document db with account $AccountName"
+New-AzResource -ResourceType "Microsoft.DocumentDb/databaseAccounts" `
+    -ApiVersion "2015-04-08" -ResourceGroupName $ResourceGroupName -Location $Location `
+    -Name $AccountName -PropertyObject $CosmosDBProperties
+Write-Host "Finished Creating document db with account $AccountName"
---
+++ angularsample/Settings/CosmosDb.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.Configuration;
+using System.IO;
+
+namespace AngularPerformanceSamples.Settings
+{
+    public class CosmosDb
+    {
+        private static IConfiguration configuration;
+        static CosmosDb()
+        {
+            var builder = new ConfigurationBuilder()
+          .SetBasePath(Directory.GetCurrentDirectory())
+          .AddJsonFile("cosmosdb.json", optional: true, reloadOnChange: true);
+            configuration = builder.Build();
+
+        }
+        public static string Get(string name)
+        {
+            string appSettings = configuration[name];
+            return appSettings;
+        }
+        public string EndpointUrl { get => Get("EndpointUrl"); }
+        public string PrimaryKey { get => Get("PrimaryKey"); }
+    }
+}
