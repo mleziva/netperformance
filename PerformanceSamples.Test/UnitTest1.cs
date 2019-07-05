a960b331e2c08ba2946b44b46afa983c921fb3a0
Austin Mleziva <ausmle@microsoft.com>
2019-07-05 10:44:33 -0500

    Adding run settings file to unit test

--- PerformanceSamples.Test/UnitTest1.cs
+++ PerformanceSamples.Test/UnitTest1.cs
@@ -2,6 +2,7 @@ using AngularPerformanceSamples.Models;
 using AngularPerformanceSamples.Services;
 using AngularPerformanceSamples.Settings;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
 using System.Diagnostics;
 using System.Threading.Tasks;
 
@@ -10,12 +11,21 @@ namespace PerformanceSamples.Test
     [TestClass]
     public class UnitTest1
     {
+        private static TestContext _testContext;
+        [ClassInitialize]
+        public static void SetupTests(TestContext testContext)
+        {
+            _testContext = testContext;
+        }
         [TestMethod]
-        [DeploymentItem("./Settings/cosmosdb.json")]
         public async Task TestMethod1()
         {
-            var dbConfig = new CosmosDbConfig();
-            var dbClient = new CosmosDbClient(dbConfig);
+            string endpointUrl = _testContext.Properties["EndpointUrl"].ToString();
+            string primaryKey = _testContext.Properties["PrimaryKey"].ToString();
+            var dbconfigMock = new Mock<ICosmosDbConfig>();
+            dbconfigMock.SetupGet(x => x.EndpointUrl).Returns(endpointUrl);
+            dbconfigMock.SetupGet(x => x.PrimaryKey).Returns(primaryKey);
+            var dbClient = new CosmosDbClient(dbconfigMock.Object);
             var document = new DocumentObject("Test-2", "smalldata1");
             await dbClient.CreateOrSetDatabase("db1");
             await dbClient.CreateCollection("collection1");
